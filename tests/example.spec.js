acaa595680d8adaaf446b4ea22bf544aaebe2f25
Anjali1003 <anjalibogam88@gmail.com>
2025-09-23 00:26:33 +0530

    Initial commit

---
+++ tests/example.spec.js
@@ -0,0 +1,32 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+import {ai} from "@zerostep/playwright"
+
+test('AI Test capability', async ({ page }) => {
+  const aiArgs = {page,test}
+  await page.goto('https://rahulshettyacademy.com/seleniumPractise/#/offers');
+  const discountPrice = await ai("What is the Discount price of Tomato",aiArgs)
+  expect(discountPrice).toEqual("26")
+  const price = await ai("What is the Price of Tomato",aiArgs)
+  expect(price).toEqual("37")
+  const diff = await ai("What is the value difference between price and Discounr price of tomato",aiArgs)
+  expect(diff).toEqual("11")
+
+  await page.goto("https://rahulshettyacademy.com/dropdownsPractise/")
+  const blinkingText = await ai("Get blinkingText in the page",aiArgs)
+  expect(blinkingText).toEqual("London QA Meetup @Rahul Shetty - Limited Seats! Book Now!")
+
+
+  
+
+
+
+
+
+
+
+
+
+
+});
+
